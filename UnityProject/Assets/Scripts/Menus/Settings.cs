50be6d39ee404a738b79dcba53cb51b1c4258b99
soccergrl5 <101655846+soccergrl5@users.noreply.github.com>
2026-08-28 17:38:20 +0200

    Turn up those Voices

---
+++ UnityProject/Assets/Scripts/Menus/Settings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WiesnKrisn.Menus
+{
+    public class Settings : MonoBehaviour
+    {
+        public static Settings Instance {get; private set;}
+        
+        [SerializeField] private Slider volumeVoice;
+        
+        [SerializeField] private Button close;
+        
+        public const string VolumeVoiceKey = "VolumeVoice";
+        
+        private void Awake()
+        {
+            Instance = this;
+            
+            volumeVoice.onValueChanged.AddListener(volume =>
+            {
+                PlayerPrefs.SetFloat(VolumeVoiceKey, volume);
+            });
+            
+            close.onClick.AddListener(Hide);
+        }
+
+        private void Start()
+        {
+            Hide();
+            
+            if (PlayerPrefs.GetInt("StartedOnce") == 0)
+            {
+                PlayerPrefs.SetInt("StartedOnce", 1);
+
+                PlayerPrefs.SetFloat(VolumeVoiceKey, 0.5f);
+            }
+            
+            volumeVoice.value = PlayerPrefs.GetFloat(VolumeVoiceKey);
+        }
+        
+        private void Hide() => gameObject.SetActive(false);
+        public void Show() => gameObject.SetActive(true);
+    }
+}
