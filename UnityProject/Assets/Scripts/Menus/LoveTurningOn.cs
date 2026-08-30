d7c1a7b86ade4291a90cb18ecbb0858af0197719
soccergrl5 <101655846+soccergrl5@users.noreply.github.com>
2026-08-30 09:07:20 +0200

    Drama at the End

---
+++ UnityProject/Assets/Scripts/Menus/LoveTurningOn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using WiesnKrisn.Audio;
+
+namespace WiesnKrisn.Menus
+{
+    public class LoveTurningOn : MonoBehaviour
+    {
+        [SerializeField] private AudioClip buzz;
+
+        [SerializeField] private Animator credits;
+        
+        public void PlayBuzz() => SFXManager.Instance.PlayEffect(buzz);
+
+        public void EndFlicker()
+        {
+            SFXManager.Instance.PlayEffect(buzz);
+            GetComponent<Animator>().SetTrigger("End");
+            
+            MusicManager.Instance.GetComponent<AudioSource>().Play();
+            
+            credits.SetTrigger("Start");
+        }
+    }
+}
